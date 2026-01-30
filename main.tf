8e4d832729f95ae3cd901e4664d182167dadaae7
Nithintv0 <nithintv0@gmail.com>
2026-01-30 23:16:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,18 @@ resource "google_compute_instance" "low_cost_vm1" {
       type  = "pd-standard"
     }
   }
+# Low-cost instance
+resource "google_compute_instance" "low_cost_vm2" {
+  name         = "low-cost-vm2"
+  machine_type = var.machine_type
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12" # small, common Linux image
+      size  = 10                       # GB (keep small to reduce cost)
+      type  = "pd-standard"
+    }
+  }
   network_interface {
     network = "default"
     # Omit access_config {} to avoid assigning an external IP (reduces exposure
